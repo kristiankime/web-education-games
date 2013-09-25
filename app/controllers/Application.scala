864ada968ea85a11e8a7d9567f736c47fbdf50b6
Kristian Kime <kristian.kime@gmail.com>
2013-09-25 17:56:19 -0400

    merging in Chat Application code

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,12 +2,49 @@ package controllers
 
 import play.api._
 import play.api.mvc._
-import securesocial.core.{Identity, Authorization}
+import securesocial.core.{ Identity, Authorization }
+
+import play.api._
+import play.api.mvc._
+
+import play.api.libs.json._
+import play.api.libs.iteratee._
+
+import models._
+
+import akka.actor._
+import scala.concurrent.duration._
 
 object Application extends Controller with securesocial.core.SecureSocial {
-  
-  def index = SecuredAction { implicit request =>
-    Ok(views.html.index("Your new application is ready."))
-  }
-  
+
+	//	def index = SecuredAction { implicit request =>
+	//		Ok(views.html.index("Your new application is ready."))
+	//	}
+
+	/**
+	 * Just display the home page.
+	 */
+	def index = Action { implicit request =>
+		Ok(views.html.index())
+	}
+
+	/**
+	 * Display the chat room page.
+	 */
+	def chatRoom(username: Option[String]) = Action { implicit request =>
+		username.filterNot(_.isEmpty).map { username =>
+			Ok(views.html.chatRoom(username))
+		}.getOrElse {
+			Redirect(routes.Application.index).flashing(
+				"error" -> "Please choose a valid username.")
+		}
+	}
+
+	/**
+	 * Handles the chat websocket.
+	 */
+	def chat(username: String) = WebSocket.async[JsValue] { request =>
+		ChatRoom.join(username)
+	}
+
 }

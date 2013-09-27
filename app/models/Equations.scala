3025499ad95e844da58db5718f23ac84c23aed3d
Kristian Kime <kristian.kime@gmail.com>
2013-09-27 15:55:03 -0400

    equations are now displayed via MathJax

--- app/models/Equations.scala
+++ app/models/Equations.scala
@@ -5,13 +5,15 @@ import scala.collection.mutable.LinkedHashMap
 case class Equation(id: Int, equation: String)
 
 object Equations {
+	private var idCounter = 0
 	private val equations = LinkedHashMap[Int, Equation]()
 	
 	def all() = equations.values.toList
 
 	def create(equation: String) = {
-		val id = equations.size;
-		val eq = Equation(id, equation);
+		val id = idCounter
+		idCounter += 1
+		val eq = Equation(id, equation)
 		equations.put(id, eq)
 		eq
 	}

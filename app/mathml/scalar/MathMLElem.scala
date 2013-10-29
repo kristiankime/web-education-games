d2a5f69c76484346fcfc9467c9617e6f3b204aa3
Kristian Kime <kristian.kime@gmail.com>
2013-10-28 21:18:17 -0400

    operator getting its own file

--- app/mathml/scalar/MathMLElem.scala
+++ app/mathml/scalar/MathMLElem.scala
@@ -4,7 +4,6 @@ import scala.util._
 import scala.xml._
 import mathml._
 
-
 abstract class MathMLElem(
 	prefix: String,
 	label: String,
@@ -16,14 +15,14 @@ abstract class MathMLElem(
 
 	def eval(boundVariables: Map[String, Double]): Try[Double]
 
-	def isZero: Boolean = if(cn.nonEmpty) cn == Cn(0) else false
+	def isZero: Boolean = if (cn.nonEmpty) cn == Cn(0) else false
 
-	def isOne: Boolean = if(cn.nonEmpty) cn == Cn(1) else false
+	def isOne: Boolean = if (cn.nonEmpty) cn == Cn(1) else false
 
 	def cn: Option[Cn]
 
 	def simplify(): MathMLElem
-	
+
 	def variables: Set[String]
 
 	def derivative(wrt: String): MathMLElem
@@ -32,105 +31,14 @@ abstract class MathMLElem(
 
 	def dx = derivative("x")
 
-	def +(m : MathMLElem) = ApplyPlus(this, m)
+	def +(m: MathMLElem) = ApplyPlus(this, m)
 
-	def *(m : MathMLElem) = ApplyTimes(this, m)
+	def *(m: MathMLElem) = ApplyTimes(this, m)
 
-	def -(m : MathMLElem) = ApplyMinusB(this, m)
+	def -(m: MathMLElem) = ApplyMinusB(this, m)
 
-	def /(m : MathMLElem) = ApplyDivide(this, m)
+	def /(m: MathMLElem) = ApplyDivide(this, m)
 
-	def ^(m : MathMLElem) = ApplyPower(this, m)
-}
-
-abstract class Applyable(
-	override val prefix: String,
-	override val label: String,
-	attributes1: MetaData,
-	override val scope: NamespaceBinding)
-	extends MathMLElem(prefix, label, attributes1, scope, true, Seq(): _*) {
-
-	def eval(boundVariables: Map[String, Double]) = Failure(new UnsupportedOperationException("Applyables should not get evaled, use eval on the surrounding apply element."))
-	
-	def cn: Option[Cn] = None
-
-	def variables: Set[String] = Set()
-	
-	def derivative(wrt: String): MathMLElem = throw new UnsupportedOperationException("Applyables should not get derived, use derive on the surrounding apply element.")
-}
-
-case class Plus(
-	override val prefix: String,
-	attributes1: MetaData,
-	override val scope: NamespaceBinding)
-	extends Applyable(prefix, "plus", attributes1, scope) {
-
-	def this() = this(MathML.h.prefix, MathML.h.attributes, MathML.h.scope)
-
-	def simplify() = this
-}
-
-object Plus {
-	def apply() = new Plus()
-}
-
-case class Minus(
-	override val prefix: String,
-	attributes1: MetaData,
-	override val scope: NamespaceBinding)
-	extends Applyable(prefix, "minus", attributes1, scope) {
-
-	def this() = this(MathML.h.prefix, MathML.h.attributes, MathML.h.scope)
-
-	def simplify() = this
-}
-
-object Minus {
-	def apply() = new Minus()
-}
-
-case class Times(
-	override val prefix: String,
-	attributes1: MetaData,
-	override val scope: NamespaceBinding)
-	extends Applyable(prefix, "times", attributes1, scope) {
-
-	def this() = this(MathML.h.prefix, MathML.h.attributes, MathML.h.scope)
-
-	def simplify() = this
-}
-
-object Times {
-	def apply() = new Times()
-}
-
-case class Divide(
-	override val prefix: String,
-	attributes1: MetaData,
-	override val scope: NamespaceBinding)
-	extends Applyable(prefix, "divide", attributes1, scope) {
-
-	def this() = this(MathML.h.prefix, MathML.h.attributes, MathML.h.scope)
-
-	def simplify() = this
-}
-
-object Divide {
-	def apply() = new Divide()
-}
-
-case class Power(
-	override val prefix: String,
-	attributes1: MetaData,
-	override val scope: NamespaceBinding)
-	extends Applyable(prefix, "power", attributes1, scope) {
-
-	def this() = this(MathML.h.prefix, MathML.h.attributes, MathML.h.scope)
-
-	def simplify() = this
-}
-
-object Power {
-	def apply() = new Power()
+	def ^(m: MathMLElem) = ApplyPower(this, m)
 }
 
---
+++ app/mathml/scalar/Operator.scala
@@ -0,0 +1,96 @@
+package mathml.scalar
+
+import scala.util._
+import scala.xml._
+import mathml._
+
+abstract class Operator(
+	override val prefix: String,
+	override val label: String,
+	attributes1: MetaData,
+	override val scope: NamespaceBinding)
+	extends MathMLElem(prefix, label, attributes1, scope, true, Seq(): _*) {
+
+	def eval(boundVariables: Map[String, Double]) = Failure(new UnsupportedOperationException("Applyables should not get evaled, use eval on the surrounding apply element."))
+
+	def cn: Option[Cn] = None
+
+	def variables: Set[String] = Set()
+
+	def derivative(wrt: String): MathMLElem = throw new UnsupportedOperationException("Applyables should not get derived, use derive on the surrounding apply element.")
+}
+
+case class Plus(
+	override val prefix: String,
+	attributes1: MetaData,
+	override val scope: NamespaceBinding)
+	extends Operator(prefix, "plus", attributes1, scope) {
+
+	def this() = this(MathML.h.prefix, MathML.h.attributes, MathML.h.scope)
+
+	def simplify() = this
+}
+
+object Plus {
+	def apply() = new Plus()
+}
+
+case class Minus(
+	override val prefix: String,
+	attributes1: MetaData,
+	override val scope: NamespaceBinding)
+	extends Operator(prefix, "minus", attributes1, scope) {
+
+	def this() = this(MathML.h.prefix, MathML.h.attributes, MathML.h.scope)
+
+	def simplify() = this
+}
+
+object Minus {
+	def apply() = new Minus()
+}
+
+case class Times(
+	override val prefix: String,
+	attributes1: MetaData,
+	override val scope: NamespaceBinding)
+	extends Operator(prefix, "times", attributes1, scope) {
+
+	def this() = this(MathML.h.prefix, MathML.h.attributes, MathML.h.scope)
+
+	def simplify() = this
+}
+
+object Times {
+	def apply() = new Times()
+}
+
+case class Divide(
+	override val prefix: String,
+	attributes1: MetaData,
+	override val scope: NamespaceBinding)
+	extends Operator(prefix, "divide", attributes1, scope) {
+
+	def this() = this(MathML.h.prefix, MathML.h.attributes, MathML.h.scope)
+
+	def simplify() = this
+}
+
+object Divide {
+	def apply() = new Divide()
+}
+
+case class Power(
+	override val prefix: String,
+	attributes1: MetaData,
+	override val scope: NamespaceBinding)
+	extends Operator(prefix, "power", attributes1, scope) {
+
+	def this() = this(MathML.h.prefix, MathML.h.attributes, MathML.h.scope)
+
+	def simplify() = this
+}
+
+object Power {
+	def apply() = new Power()
+}

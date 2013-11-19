83a8d0abc2afdd3b11c3fadf70e5175ab9e8d639
Kristian Kime <kristian.kime@gmail.com>
2013-11-19 12:14:32 -0500

    adding tests for Sin/Cos

---
+++ test/mathml/scalar/apply/ApplyCosSpec.scala
@@ -0,0 +1,58 @@
+package mathml.scalar.apply
+
+import org.junit.runner.RunWith
+import play.api.test._
+import play.api.test.Helpers._
+import org.specs2.mutable._
+import mathml._
+import mathml.scalar._
+import org.junit.runner.RunWith
+import org.specs2.runner.JUnitRunner
+
+// LATER try out http://rlegendi.github.io/specs2-runner/ and remove RunWith
+@RunWith(classOf[JUnitRunner])
+class ApplyCosSpec extends Specification {
+
+	"eval" should {
+		"do sin" in {
+			ApplyCos(π).eval(Map()).get must beEqualTo(-1)
+		}
+	}
+
+	"variables" should {
+		"be empty if elements are constant" in {
+			ApplyCos(`2`).variables must beEmpty
+		}
+
+		"be x if an element constains an x" in {
+			ApplyCos(x).variables must beEqualTo(Set("x"))
+		}
+	}
+
+	"c" should {
+		"return correct sin" in {
+			ApplyCos(π).c.get must beEqualTo(`-1`)
+		}
+
+		"fail if not a constant " in {
+			ApplyCos(x).c must beEmpty
+		}
+	}
+
+	//	"s" should {
+	//		"return constant if value is constant" in {
+	//			ApplyCos(e).s must beEqualTo(`1`)
+	//		}
+	//
+	//		"remain unchanged if nothing can be simplified" in {
+	//			ApplyCos(x).s must beEqualTo(ApplyCos(x))
+	//		}
+	//	}
+
+	"d" should {
+		"obey the sin derivative rule: ln(f)' = f'/f" in {
+			ApplyCos(F).dx must beEqualTo(-ApplySin(F) * Fdx)
+		}
+	}
+
+}
---
+++ test/mathml/scalar/apply/ApplySinSpec.scala
@@ -0,0 +1,63 @@
+package mathml.scalar.apply
+
+import org.junit.runner.RunWith
+import play.api.test._
+import play.api.test.Helpers._
+import org.specs2.mutable._
+import mathml._
+import mathml.scalar._
+import org.junit.runner.RunWith
+import org.specs2.runner.JUnitRunner
+
+// LATER try out http://rlegendi.github.io/specs2-runner/ and remove RunWith
+@RunWith(classOf[JUnitRunner])
+class ApplySinSpec extends Specification {
+
+	"eval" should {
+		"do sin" in {
+			ApplySin(π / `2`).eval(Map()).get must beEqualTo(1)
+		}
+	}
+
+	"variables" should {
+		"be empty if elements are constant" in {
+			ApplySin(`2`).variables must beEmpty
+		}
+
+		"be x if an element constains an x" in {
+			ApplySin(x).variables must beEqualTo(Set("x"))
+		}
+	}
+
+	"c" should {
+		"return correct sin" in {
+			ApplySin(π / `2`).c.get must beEqualTo(`1`)
+		}
+
+		"fail if not a constant " in {
+			ApplySin(x).c must beEmpty
+		}
+	}
+
+	//	"s" should {
+	//		"return constant if value is constant" in {
+	//			ApplySin(e).s must beEqualTo(`1`)
+	//		}
+	//
+	//		"remain unchanged if nothing can be simplified" in {
+	//			ApplySin(x).s must beEqualTo(ApplySin(x))
+	//		}
+	//	}
+
+	"d" should {
+		"obey the sin derivative rule: ln(f)' = f'/f" in {
+			ApplySin(F).dx must beEqualTo(ApplyCos(F) * Fdx)
+		}
+	}
+
+}
+
+
+
+
+

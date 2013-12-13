e275e40464222950ee03648aeeea40ccdd4b6908
Kristian Kime <kristian.kime@gmail.com>
2013-12-13 15:50:53 -0500

    added simple error handling pages

---
+++ app/Global.scala
@@ -0,0 +1,20 @@
+import play.api._
+import play.api.mvc._
+import play.api.mvc.Results._
+import scala.concurrent.Future
+
+object Global extends GlobalSettings {
+
+	override def onBadRequest(request: RequestHeader, error: String) = {
+		Future.successful(BadRequest("Bad Request: " + error))
+	}
+
+	override def onError(request: RequestHeader, ex: Throwable) = {
+		Future.successful(InternalServerError(views.html.errors.errorPage(ex)))
+	}
+
+	override def onHandlerNotFound(request: RequestHeader) = {
+		Future.successful(NotFound(views.html.errors.notFoundPage(request.path)))
+	}
+
+}

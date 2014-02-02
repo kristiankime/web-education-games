af279d2c80605346e2dbf6b8b1e99708d7f46afd
Kristian Kime <kristian.kime@gmail.com>
2014-02-01 21:13:33 -0500

    adding email env variables to heroku example

--- heroku_environment_variables_EXAMPLE.sh
+++ heroku_environment_variables_EXAMPLE.sh
@@ -7,6 +7,10 @@ heroku config:set FACEBOOK_APP_SECRET=put_app_secret_here
 heroku config:set GOOGLE_CLIENT_ID=put_google_client_id_here
 heroku config:set GOOGLE_SECRET=put_google_secret_here
 
+heroku config:set EMAIL_USER=put_user_here
+heroku config:set EMAIL_PASSWORD=put_password_here
+heroku config:set EMAIL_ADDRESS=put_address_here
+
 # Database configuration
 heroku config:set DATABASE_DRIVER=put_db_driver_here
 # DATABASE_URL is usually already set by heroku 

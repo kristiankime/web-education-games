02943fa1694e2639aae37e15c53176531768e04a
Kristian Kime <kristian.kime@gmail.com>
2014-03-01 21:05:02 -0500

    quiz view now show student results

--- app/models/question/derivative/view/QuizDetails.scala
+++ app/models/question/derivative/view/QuizDetails.scala
@@ -5,6 +5,12 @@ import service.User
 
 case class QuizDetails(quiz: Quiz, studentResults: List[StudentResults])
 
-case class StudentResults(student: User, studentQuestions: List[StudentQuestionResults])
+case class StudentResults(student: User, studentQuestions: List[StudentQuestionResults]) {
+	def numQuestions = studentQuestions.size
+	def numCorrect = studentQuestions.map(s => if (s.correct) { 1 } else { 0 }).foldLeft(0)(_ + _)
+	def numAttempted = studentQuestions.map(s => if (s.attempted) { 1 } else { 0 }).foldLeft(0)(_ + _)
+}
 
-case class StudentQuestionResults(question: Question, correct: Boolean, answers: List[Answer])
+case class StudentQuestionResults(question: Question, correct: Boolean, answers: List[Answer]) {
+	def attempted = answers.nonEmpty
+}

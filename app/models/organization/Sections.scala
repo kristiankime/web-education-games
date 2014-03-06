8b5ffc9e3a0450789ea2b02e26c3542c6e67f4d3
Kristian Kime <kristian.kime@gmail.com>
2014-03-05 20:01:02 -0500

    student show up sorted by email in section results

--- app/models/organization/Sections.scala
+++ app/models/organization/Sections.scala
@@ -63,7 +63,7 @@ object Sections {
 		(for (
 			u <- (new UserTable);
 			us <- (new UsersSectionsTable) if us.userId === u.id && us.id === sectionId && us.access === service.Access.view
-		) yield u).list
+		) yield u).sortBy(_.email).list
 
 	// ======= AUTHORIZATION ======
 	def otherAccess(user: User, sectionId: SectionId)(implicit session: Session) =

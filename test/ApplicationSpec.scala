f271f21f208b7b19c6d4e803b3e7b7d4f733b1f8
Kristian Kime <kristian.kime@gmail.com>
2014-04-04 09:25:51 -0400

    switching fakeUser to require a session

--- test/ApplicationSpec.scala
+++ test/ApplicationSpec.scala
@@ -5,11 +5,16 @@ import play.api.test._
 import play.api.test.Helpers._
 import com.artclod.securesocial.TestUtils._
 import models.DBTest
-import service.UserTmpTest
+import models.DBTest.fakeUser
+import models.organization.Courses
+import models.organization.CourseTmpTest
+import service.{UserTmpTest => U}
 import securesocial.core.PasswordInfo
 import service.SlickUserService
 import securesocial.core.Identity
 import securesocial.core.IdGenerator
+import play.api.db.slick.DB
+import scala.slick.session.Session
 
 @RunWith(classOf[JUnitRunner])
 class ApplicationSpec extends Specification {
@@ -21,12 +26,27 @@ class ApplicationSpec extends Specification {
 		}
 
 		"render the index page" in new WithApplication {
-			val user = DBTest.fakeUser(UserTmpTest())
-			val home = route(FakeRequest(GET, "/").withLoggedInUser(user)).get
+			DB.withSession { implicit session: Session =>
+				val user = fakeUser
+				val home = route(FakeRequest(GET, "/").withLoggedInUser(user)).get
 
-			status(home) must equalTo(OK)
-			contentType(home) must beSome.which(_ == "text/html")
-			contentAsString(home) must contain("Welcome to the EdTech server")
+				status(home) must equalTo(OK)
+				contentType(home) must beSome.which(_ == "text/html")
+				contentAsString(home) must contain("Welcome to the EdTech server")
+			}
 		}
+	}
+
+	"Courses" should {
+
+		"send allow any user to view a course" in new WithApplication {
+			DB.withSession { implicit session: Session =>
+				val user = fakeUser
+				val course = Courses.create(CourseTmpTest(owner = fakeUser.id))
+				
+				val home = route(FakeRequest(GET, "/courses/" + course.id.v).withLoggedInUser(user)).get
+			}
+		}
+
 	}
 }
--- test/models/DBTest.scala
+++ test/models/DBTest.scala
@@ -1,6 +1,6 @@
 package models
 
-import play.api.db.slick.DB
+//import play.api.db.slick.DB
 import scala.slick.session.Session
 import play.api.test.Helpers.inMemoryDatabase
 import play.api.test.FakeApplication
@@ -8,13 +8,14 @@ import service.UserTmp
 import play.api.Play.current
 import play.api.db.slick.DB
 import service.table.UserTable
+import service.UserTmpTest
+import service.User
 
 object DBTest {
 
 	val inMemH2 = inMemoryDatabase(options = Map("MODE" -> "PostgreSQL"))
 
-	def fakeUser(data: UserTmp) = DB.withSession { implicit session: Session =>
-		(new UserTable).insert(data)
-	}
+	def fakeUser(data: UserTmp)(implicit session: Session): User = (new UserTable).insert(data)
 
+	def fakeUser(implicit session: Session): User = fakeUser(UserTmpTest())
 }

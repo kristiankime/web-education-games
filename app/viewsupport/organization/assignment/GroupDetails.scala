8509cd95fbdf9052dfaf283f83940c8d3479256d
Kristian Kime <kristian.kime@gmail.com>
2014-05-09 07:28:03 -0400

    cleaning up links and removing join link if they user is in a group

--- app/viewsupport/organization/assignment/GroupDetails.scala
+++ app/viewsupport/organization/assignment/GroupDetails.scala
@@ -3,4 +3,6 @@ package viewsupport.organization.assignment
 import models.organization.assignment.Group
 import service.User
 
-case class GroupDetails(group: Group, students: List[User])
+case class GroupDetails(group: Group, students: List[User]) {
+  def enrolled(implicit user: User) = students.contains(user)
+}

4adcb8cc57a07982d55c5472c29321d053ee74f5
Kristian Kime <kristian.kime@gmail.com>
2014-06-11 22:15:12 -0400

    more cleanup of files

--- app/viewsupport/question/derivative/QuizResults.scala
+++ app/viewsupport/question/derivative/QuizResults.scala
@@ -1,38 +1,9 @@
 package viewsupport.question.derivative
 
-import com.artclod.collection.PimpedSeq
-import play.api.db.slick.Config.driver.simple._
 import models.question.derivative._
-import service.User
-
 
 case class QuizResults(quiz: Quiz, results: List[UserQuizResults])
 
-case class UserQuizResults(user: User, quiz: Quiz, results: List[QuestionResults]) {
-  def numQuestions = results.size
-
-  val numCorrect = results.map(s => if (s.correct) 1 else 0).sum
-
-  val numAttempted = results.map(s => if (s.attempted) 1 else 0).sum
-
-  val questions = results.map(_.question)
-
-  def previousQuestion(question: Question) = questions.elementBefore(question)
-
-  def nextQuestion(question: Question) = questions.elementAfter(question)
-
-  val score = {
-    val scores = results.flatMap(_.score)
-    if (scores.isEmpty) None
-    else Some(scores.sum / scores.size)
-  }
-
-  def teacherScore(implicit session: Session): Option[Double] = {
-    val teacherScores = results.flatMap(_.teacherScore)
-    if (teacherScores.isEmpty) None
-    else Some(teacherScores.sum / teacherScores.size)
-  }
-}
 
 
 
---
+++ app/viewsupport/question/derivative/UserQuizResults.scala
@@ -0,0 +1,32 @@
+package viewsupport.question.derivative
+
+import com.artclod.collection.PimpedSeq
+import play.api.db.slick.Config.driver.simple._
+import models.question.derivative.{Question, Quiz}
+import service.User
+
+case class UserQuizResults(user: User, quiz: Quiz, results: List[QuestionResults]) {
+  def numQuestions = results.size
+
+  val numCorrect = results.map(s => if (s.correct) 1 else 0).sum
+
+  val numAttempted = results.map(s => if (s.attempted) 1 else 0).sum
+
+  val questions = results.map(_.question)
+
+  def previousQuestion(question: Question) = questions.elementBefore(question)
+
+  def nextQuestion(question: Question) = questions.elementAfter(question)
+
+  val score = {
+    val scores = results.flatMap(_.score)
+    if (scores.isEmpty) None
+    else Some(scores.sum / scores.size)
+  }
+
+  def teacherScore(implicit session: Session): Option[Double] = {
+    val teacherScores = results.flatMap(_.teacherScore)
+    if (teacherScores.isEmpty) None
+    else Some(teacherScores.sum / teacherScores.size)
+  }
+}

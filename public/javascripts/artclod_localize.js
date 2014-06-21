516bf67012a0c20eeaa24ab737414ae2a7828bbd
Kristian Kime <kristian.kime@gmail.com>
2014-06-21 12:08:29 -0400

    times show seconds

--- public/javascripts/artclod_localize.js
+++ public/javascripts/artclod_localize.js
@@ -4,5 +4,5 @@ if (!ARTC) {
 }
 
 ARTC.localize = function (t) {
-    document.write(moment(new Date(t + " UTC")).calendar());
+    document.write(moment(new Date(t + " UTC")).format("M/D/YYYY h:mm:ss a"));
 }

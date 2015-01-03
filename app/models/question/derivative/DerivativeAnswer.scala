0023353c0de5135f060700d3e24ec1d7d2636e61
Kristian Kime <kristian.kime@gmail.com>
2015-01-02 20:56:30 -0500

    answer editor now separate view

--- app/models/question/derivative/DerivativeAnswer.scala
+++ app/models/question/derivative/DerivativeAnswer.scala
@@ -4,6 +4,7 @@ import com.artclod.mathml.scalar.MathMLElem
 import models.question.ViewableMath
 import models.support.{Owned, AnswerId, QuestionId, UserId}
 import org.joda.time.DateTime
+import play.api.templates.Html
 
 case class DerivativeAnswer(id: AnswerId, ownerId: UserId, questionId: QuestionId, mathML: MathMLElem, rawStr: String, correctNum: Short, creationDate: DateTime) extends ViewableMath with Owned {
   // We need to count number of correct answers in the db, so we store correct as a number with { 0 -> false, 1 -> true }
@@ -16,6 +17,9 @@ case class DerivativeAnswer(id: AnswerId, ownerId: UserId, questionId: QuestionI
   }
 
   private def correctNumError = throw new IllegalStateException("In " + this + " correctNum was [" + correctNum + "] can only be in { 0 -> false, 1 -> true }, coding error")
+
+  // === Methods for viewing
+  def display : Html = views.html.mathml.mathmlDisplay(this)
 }
 
 object DerivativeAnswerUnfinished {

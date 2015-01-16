0379807d36fb9d0df33420859ecec45380790c9b
Kristian Kime <kristian.kime@gmail.com>
2015-01-16 14:54:43 -0500

    refactoring for leader board code and fixing a resource loading bug

--- app/com/artclod/javascript/RunJS.scala
+++ app/com/artclod/javascript/RunJS.scala
@@ -8,11 +8,12 @@ import play.api.Play.current
 
 // modified from https://github.com/mozilla/rhino/blob/master/examples/RunScript.java
 object RunJS {
+
   // Rhino code modified from from http://stackoverflow.com/questions/650377/javascript-rhino-use-library-or-include-other-scripts
-  // Play resource code modified from http://stackoverflow.com/questions/27115924/play-framework-get-asset-server-side
   def loadJSLibrary(pathToLib: String, libName: String)(implicit context: Context, scope: ScriptableObject) = {
-    val file = Play.application.getExistingFile(pathToLib + libName).get // TODO better error handling
-    val fileInputStream = new FileInputStream(file)
+    // http://stackoverflow.com/questions/23120498/playframework-heroku-cannot-find-assets
+    val fileInputStream = Play.application.classloader.getResourceAsStream(pathToLib + libName)
+    if(fileInputStream == null) { throw new IllegalStateException("Was unable to find file for JS Library at [" + pathToLib + libName + "] ") }
     val inputStreamReader = new InputStreamReader(fileInputStream)
     context.evaluateReader(scope, inputStreamReader, libName, 1, null)
   }
@@ -21,7 +22,7 @@ object RunJS {
     implicit val cx = Context.enter()
     try {
       implicit val scope = cx.initStandardObjects()
-      loadJSLibrary("/public/javascripts/mathjs/", "math.js")
+      loadJSLibrary("public/javascripts/mathjs/", "math.js")
       val result = cx.evaluateString(scope, "math.parse(\""  + math + "\").toTex()", "<cmd>", 1, null)
       Context.toString(result)
     } finally {
--- app/models/tournament/Tournaments.scala
+++ app/models/tournament/Tournaments.scala
@@ -10,36 +10,59 @@ import models.quiz.Correct2Short
 
 object Tournaments {
 
-  def studentScores(implicit session: Session) = {
-    val usersAndAnswers = studentQuestionsAnswered
-    val user2Difficulty = usersAndAnswers.groupBy(r => (r._1, r._3)).mapValues(_.map(_._2))
-    val user2HighestDifficulty = user2Difficulty.mapValues(l => l.sorted(Ordering[Double].reverse).take(5))
-    val user2Score = user2HighestDifficulty.mapValues(v => v.sum / v.length)
-    val usersAndScores = user2Score.toList.map(e => Score(e._1._1, e._2, e._1._2)).sortBy(_.score)((Ordering[Double].reverse)).take(5)
-    usersAndScores
+  // ============ Generic Ranking Core ===========
+  case class Rankings[M](ranks: List[Rank[M]], user: Option[Rank[M]])
+
+  case class Rank[M](id:UserId, name: String, metric: M, index: Int)
+
+  private def rankingsFor[M](id:UserId, size: Int, ranks:  List[Rank[M]]) = {
+    val findUserRank = ranks.find(_.id == id)
+    val userRank = findUserRank.flatMap(e => if(e.index > size){ Some(e) } else { None })
+    Rankings[M](ranks.take(size), userRank)
   }
 
-  def studentQuestionsAnswered(implicit session: Session) = {
+  // ============ Student Scores Rankings ===========
+  def studentScoresRankingFor(id:UserId, size: Int)(implicit session: Session) = {
+    rankingsFor(id, size, studentScoresRankings)
+  }
+
+  private def studentScoresRankings(implicit session: Session) = {
+    val usersAndAnswers = questionsAnsweredCorrectly
+    val user2Difficulty = usersAndAnswers.groupBy(r => (r._1, r._2)).mapValues(_.map(_._3))
+    val user2HighestDifficulty = user2Difficulty.mapValues(l => l.sorted(Ordering[Double].reverse).take(5))
+    val user2HighScoreAverage = user2HighestDifficulty.mapValues(v => v.sum / v.length)
+    val usersAndScores = user2HighScoreAverage.toList.map(e => (e._1._1, e._1._2, e._2)).sortBy(_._3)((Ordering[Double].reverse))
+    val rankings = usersAndScores.zipWithIndex.map(v => Rank[Double](v._1._1, v._1._2, v._1._3, v._2))
+    rankings
+  }
+
+  private def questionsAnsweredCorrectly(implicit session: Session) = {
     val correctAnswers /* one entry per question + user */ = derivativeAnswersTable.filter(_.correct === Correct2Short.T).groupBy(g => (g.ownerId, g.questionId)).map{ case ((ownerId, questionId), group) => (ownerId, questionId) }
     val questionsAnsweredCorrectly = correctAnswers innerJoin derivativeQuestionsTable on (_._2 === _.id)
     val userAndQuestionDifficulty = questionsAnsweredCorrectly.map { r => (r._1._1, r._2.atCreationDifficulty) }.sortBy(r => (r._1, r._2))
-    val userNamesAndDifficulty = for { (q, s) <- userAndQuestionDifficulty innerJoin userSettingsTable on (_._1=== _.userId) } yield (q._1, q._2, s.name)
+    val userNamesAndDifficulty = for { (q, s) <- userAndQuestionDifficulty innerJoin userSettingsTable on (_._1=== _.userId) } yield (q._1, s.name, q._2)
     userNamesAndDifficulty.list
   }
 
-  case class Score(id:UserId, score:Double, name: String)
+  // ============ Number of Games Completed Rankings ===========
+  def completedGamesRankingFor(id:UserId, size: Int)(implicit session: Session) = {
+    rankingsFor(id, size, completedGamesRank)
+  }
 
-  def numberOfCompletedGamesByPlayer(implicit session: Session) = {
+  private def completedGamesRank(implicit session: Session) = {
+    val completedGames = numberOfCompletedGamesByPlayer
+    val gamesRankings = completedGames.zipWithIndex.map(v => Rank(v._1._1, v._1._2, v._1._3.getOrElse(0), v._2))
+    gamesRankings
+  }
+
+  private def numberOfCompletedGamesByPlayer(implicit session: Session) = {
     val requstorCounts = gamesTable.filter(_.finishedDate.isNotNull).groupBy(g => g.requestor).map{ case (requestor, group) => (requestor, group.length) }
     val requsteeCounts = gamesTable.filter(_.finishedDate.isNotNull).groupBy(g => g.requestee).map{ case (requestee, group) => (requestee, group.length) }
     val countsUnion = requstorCounts.unionAll(requsteeCounts)
     val counts = countsUnion.groupBy(g => g._1).map{ case (id, group) => (id, group.map(_._2).sum) }
-    val joinNames = for { (c, s) <- counts innerJoin userSettingsTable on (_._1 === _.userId) } yield (c._1, c._2, s.name)
-    val limitAndSort = joinNames.sortBy(_._2) //.take(10)
-
-    limitAndSort.list.map(r => Count(r._1, r._2.getOrElse(0), r._3))
+    val joinNames = for { (c, s) <- counts innerJoin userSettingsTable on (_._1 === _.userId) } yield (c._1, s.name, c._2)
+    val sorted = joinNames.sortBy(_._2)
+    sorted.list
   }
-
-  case class Count(id:UserId, count:Int, name: String)
 
 }

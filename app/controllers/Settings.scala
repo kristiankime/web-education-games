245275e20192bb68fb8f07671a61b8f22fde3e66
Kristian Kime <kristian.kime@gmail.com>
2015-01-24 22:48:37 -0500

    adding ability to change user name

--- app/controllers/Settings.scala
+++ app/controllers/Settings.scala
@@ -1,18 +1,46 @@
 package controllers
 
 import controllers.support.SecureSocialConsented
-import models.user.{UserSetting, UserSettings}
+import models.user.UserSettings
+import play.api.data._
+import play.api.data.Forms._
 import play.api.mvc.Controller
-import play.api.db.slick.Config.driver.simple.Session
 
 object Settings extends Controller with SecureSocialConsented {
 
   def toggleGameEmails() = ConsentedAction { implicit request => implicit user => implicit session =>
-    val settings = (UserSettings(user.id) match {
-      case Some(setting) => UserSettings.update(setting.copy(emailGameUpdates = !setting.emailGameUpdates))
-      case None => throw new IllegalStateException("Attempted to toggle email updates for [" + user.id + "] but user had no settings")
-    })
+    val setting = user.settings
+    UserSettings.update(setting.copy(emailGameUpdates = !setting.emailGameUpdates))
     Ok(views.html.user.userInfo())
   }
 
+  private def ifSomeUpdate[C, O](copy : C, option : Option[O])(f : (C, O) => C) = if(option.isEmpty) { copy } else { f(copy, option.get) }
+
+  def updateSettings() = ConsentedAction { implicit request => implicit user => implicit session =>
+    SettingsForm.values.bindFromRequest.fold(
+      errors => {
+        BadRequest(views.html.user.userInfo(Some(errors)))
+      },
+      updates => {
+        var settings = user.settings.copy(emailGameUpdates = updates.emailGameUpdates)
+        settings = ifSomeUpdate(settings, updates.name){ (s, v) => s.copy(name = v.trim) }
+        UserSettings.update(settings)
+        Redirect(routes.Home.userInfo())
+      })
+  }
+
+}
+
+case class SettingsData(name: Option[String], emailGameUpdates: Boolean)
+
+object SettingsForm {
+  val name = "name"
+  val emailGameUpdates = "emailGameUpdates"
+
+  val values = Form(mapping(
+      name -> optional(text.verifying("Name must not be blank text", _.trim != "")),
+      emailGameUpdates -> boolean
+    )(SettingsData.apply)(SettingsData.unapply))
+
+  private def validName(name: String) = { name.trim != "" }
 }

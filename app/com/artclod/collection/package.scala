2f4981bdb778d57ec066b4aa542901abe469da98
Kristian Kime <kristian.kime@gmail.com>
2015-01-28 16:08:08 -0500

    naming and unused code cleanup

--- app/com/artclod/collection/package.scala
+++ app/com/artclod/collection/package.scala
@@ -1,29 +1,12 @@
 package com.artclod
 
-import scala.collection.TraversableLike
-import scala.collection.generic.CanBuildFrom
-
-
 package object collection {
 
-//  implicit class OptionTraversable[A, T <: Traversable[A]](val op: Option[T]) extends AnyVal {
-//
-////    (implicit bf : scala.collection.generic.CanBuildFrom[Repr, B, That])
-//
-//    def dropOption(implicit bf: CanBuildFrom[T, A, T]): T = op match {
-//        case Some(thing) => thing
-//        case None => bf(op.asInstanceOf[T]).result
-//      }
-//
-//  }
-
   implicit class PimpedOptionList[T](val op: Option[List[T]]) {
-
     def dropOption: List[T] = op match {
       case Some(list) => list
       case None => List()
     }
-
   }
 
 	implicit class PimpedSeq[E](seq: Seq[E]) {
--- app/com/artclod/time/package.scala
+++ app/com/artclod/time/package.scala
@@ -8,4 +8,5 @@ package object time {
   implicit def java2JodaDate(javaDate: java.util.Date) : DateTime = if(javaDate == null){null}else{new DateTime(javaDate)}
 
   implicit def java2JodaOptionDate(javaDate: Option[java.util.Date]) : Option[DateTime] = javaDate.map(java2JodaDate(_))
+
 }

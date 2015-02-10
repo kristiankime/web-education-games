f61ae424ff291446e751b7f105249e18585399f2
Kristian Kime <kristian.kime@gmail.com>
2015-02-09 19:41:57 -0500

    cleanup and clarification

--- app/models/quiz/question/Questions.scala
+++ app/models/quiz/question/Questions.scala
@@ -46,26 +46,26 @@ object Questions {
   // ======= GENERIC METHODS USED BY SPECIFIC QUESTION TYPE MODELS =======
   def correct[Q <: Question, QT <: QuestionsTable[Q], A <: Answer, AT <: AnswersTable[A]](userId: UserId, questionTable: TableQuery[QT], answerTable: TableQuery[AT])(implicit session: Session) = {
     // Type information provided here to help IDE
-    val query1 : Query[(QT, AT), (Q, A)] =
+    val questionsAndCorrectAnswers : Query[(QT, AT), (Q, A)] =
       for(q <- questionTable; a <- answerTable if a.ownerId === userId && q.id === a.questionId && a.correct === NumericBoolean.T) yield (q, a)
-    val query2 : Query[(Column[QuestionId], Query[(QT, AT), (Q, A)]), (QuestionId, Query[(QT, AT), (Q, A)])] =
-      query1.groupBy(_._1.id)
-    val query3 = query2.map { case (questionId, qAndA) => (questionId, qAndA.map(_._2.creationDate).min) }
-    val query4 = query3.sortBy(_._2.desc)
-    query4.list.map(r => (r._1, r._2.get))
+    val groupByQuestion : Query[(Column[QuestionId], Query[(QT, AT), (Q, A)]), (QuestionId, Query[(QT, AT), (Q, A)])] =
+      questionsAndCorrectAnswers.groupBy(_._1.id)
+    val earliestAnswerTime = groupByQuestion.map { case (questionId, qAndA) => (questionId, qAndA.map(_._2.creationDate).min) }
+    val questionAndTimeSorted = earliestAnswerTime.sortBy(_._2.desc)
+    questionAndTimeSorted.list.map(r => (r._1, r._2.get))
   }
 
 
   def incorrect[Q <: Question, QT <: QuestionsTable[Q], A <: Answer, AT <: AnswersTable[A]](userId: UserId, questionTable: TableQuery[QT], answerTable: TableQuery[AT])(implicit session: Session) = {
     // Type information provided here to help IDE
-    val query1 : Query[(QT, AT), (Q, A)] =
+    val questionAndAllAnswers : Query[(QT, AT), (Q, A)] =
       for(q <- questionTable; a <- answerTable if a.ownerId === userId && q.id === a.questionId) yield (q, a)
-    val query2 : Query[(Column[QuestionId], Query[(QT, AT), (Q, A)]), (QuestionId, Query[(QT, AT), (Q, A)])] =
-      query1.groupBy(_._1.id)
-    val query3 = query2.map { case (questionId, qAndA) => (questionId, qAndA.map(_._2.correct).max, qAndA.map(_._2.creationDate).max) }
-    val query4 = query3.filter(_._2 === NumericBoolean.F) // Only include question that have no correct answer
-    val query5 = query4.sortBy(_._3.desc)
-    query5.list.map(r => (r._1, r._3.get))
+    val groupedByQuestion : Query[(Column[QuestionId], Query[(QT, AT), (Q, A)]), (QuestionId, Query[(QT, AT), (Q, A)])] =
+      questionAndAllAnswers.groupBy(_._1.id)
+    val earliestAnswerAndCorrect = groupedByQuestion.map { case (questionId, qAndA) => (questionId, qAndA.map(_._2.correct).max, qAndA.map(_._2.creationDate).max) }
+    val allAnswersIncorrect = earliestAnswerAndCorrect.filter(_._2 === NumericBoolean.F) // Only include question that have no correct answer
+    val questionAndTimeSorted = allAnswersIncorrect.sortBy(_._3.desc)
+    questionAndTimeSorted.list.map(r => (r._1, r._3.get))
   }
 }
 

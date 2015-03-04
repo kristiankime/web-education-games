50a6cc01ddd39cc472501aaba26ac6c7a3a52abd
Kristian Kime <kristian.kime@gmail.com>
2015-03-04 13:29:11 -0500

    updating help in editor tooltip

--- public/javascripts/artclod/artclod-opentip.js
+++ public/javascripts/artclod/artclod-opentip.js
@@ -3,11 +3,12 @@ if (!ARTC) {
 }
 
 ARTC.opentipMathJS = function (elementId, parser) {
-    var symbols = _.map(parser.symbols, function(value, key, list) { return key }).reduce(function(a,b) { return a + ", " + b });
+    var symbols = _.map(parser.symbols.map, function(value, key, list) { return key }).reduce(function(a,b) { return a + ", " + b });
     var operators = _.map(parser.operators, function(value, key, list) { return key } ).reduce(function(a,b) { return a + ", " + b });
     var functions = _.map(parser.functions, function(value, key, list) { return key.split("#")[0] } ).reduce(function(a,b) { return a + ", " + b });
 
-    new Opentip(elementId, "The system understands the following symbols: " + symbols + "<br>" +
-    "You can use the following operators: " + operators + "<br>" +
-    "The following are the available functions can be used by : " + functions);
+    new Opentip(elementId, "The editor understands the following<br>" +
+    "symbols: " + symbols + "<br>" +
+    "operators: " + operators + "<br>" +
+    "functions: " + functions);
 }

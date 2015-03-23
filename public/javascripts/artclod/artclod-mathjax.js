462646d589b8a18950430ca90e46262fdb9e4faf
Kristian Kime <kristian.kime@gmail.com>
2015-03-22 21:12:42 -0400

    slightly better mathjax js updating

--- public/javascripts/artclod/artclod-mathjax.js
+++ public/javascripts/artclod/artclod-mathjax.js
@@ -16,79 +16,6 @@ ARTC.mathJax.tex = function(tex){ return '$$' + tex + '$$'; };
 ARTC.mathJax.asccii = function(asciimath){ return '`' + asciimath + '`'; };
 ARTC.mathJax.mathml = function(mathML){ return '<script type="math\/mml"> ' + mathML + ' <\/script>'; };
 
-ARTC.mathJax.updateById = (function() {
-    /*
-     * Update an HTML element with a string in content MathML format and then have MathJax render it.
-     *
-     * NOTE: This function will overwrite the inner HTML of an element.
-     *
-     * This functional is designed not to throw any errors but to call a callback function
-     * with a status object indicating success or failure. If no callback is given errors are ignored.
-     *
-     * Arguments
-     *   id - The id of the element to update
-     *   math - The Math to put into the element. this will replace the innerHTML of the element so it must
-     *          be something that MathJax can process.
-     *          For convenience ARTC.mathJax.tex will wrap TeX properly,
-     *          ARTC.mathJax.asccii will wrap AsciiMath
-     *          ARTC.mathJax.mathml will wrap MathML (Presentation or Content)
-     *   callback - function that will be called after the updated completes, it is called with finished object
-     *
-     * The finished object has three attributes
-     *
-     * success - a boolean indicating if everything went well
-     * reason - a string which can be one of:
-     *          "success" -> Indicating success, this is the only string where the success attribute will be true
-     *          "id falsey" -> The id was falsey
-     *          "math falsey" -> The math was falsey
-     *          "invalid id" -> Could not find an element associated with the given id
-     * details - an empty string on success or an error object given more details about the failure
-     */
-    var update = function (id, math, callback) {
-        var safeCallback = function () {  /* noop */; };
-        if (ARTC.isFunction(callback)) {
-            safeCallback = callback;
-        }
-
-        if (!id) {
-            safeCallback({ success: false, reason: "id falsey", details: "id was " + id });
-            return;
-        }
-
-        if (!math) {
-            safeCallback({ success: false, reason: "math falsey", details: "math was " + math });
-            return;
-        }
-
-        var elem = document.getElementById(id);
-        if (elem === null) {
-            safeCallback({ success: false, reason: "invalid id", details: "could not find element for id " + id });
-            return;
-        }
-
-        elem.innerHTML = math;
-
-        var oldDisplay = elem.style.display;
-        if(oldDisplay == "") { oldDisplay = 'inline'; }
-        if(oldDisplay == "none") {
-            MathJax.Hub.Queue(
-                [ "Typeset", MathJax.Hub, elem ],
-                safeCallback({ success: true, reason: "success", details: ""})
-            );
-        } else {
-            MathJax.Hub.Queue(
-                function () {elem.style.display='none'},
-                [ "Typeset", MathJax.Hub, elem ],
-                function () {elem.style.display=oldDisplay},
-                safeCallback({ success: true, reason: "success", details: ""})
-            );
-        }
-    }
-
-    return update;
-}());
-
-
 ARTC.mathJax.updateByElem = (function() {
     /*
      * Update an HTML element with a string in content MathML format and then have MathJax render it.
@@ -132,18 +59,17 @@ ARTC.mathJax.updateByElem = (function() {
             return;
         }
 
-        elem.innerHTML = math;
-
         var oldDisplay = elem.style.display;
-        if(oldDisplay == "") { oldDisplay = 'inline'; }
         if(oldDisplay == "none") {
             MathJax.Hub.Queue(
+                function () {elem.innerHTML = math;},
                 [ "Typeset", MathJax.Hub, elem ],
                 safeCallback({ success: true, reason: "success", details: ""})
             );
         } else {
             MathJax.Hub.Queue(
                 function () {elem.style.display='none'},
+                function () {elem.innerHTML = math;},
                 [ "Typeset", MathJax.Hub, elem ],
                 function () {elem.style.display=oldDisplay},
                 safeCallback({ success: true, reason: "success", details: ""})
@@ -154,8 +80,7 @@ ARTC.mathJax.updateByElem = (function() {
     return update;
 }());
 
-
-ARTC.mathJax.updateByElem2 = (function() {
+ARTC.mathJax.updateById = (function() {
     /*
      * Update an HTML element with a string in content MathML format and then have MathJax render it.
      *
@@ -165,7 +90,7 @@ ARTC.mathJax.updateByElem2 = (function() {
      * with a status object indicating success or failure. If no callback is given errors are ignored.
      *
      * Arguments
-     *   elem - The element to update
+     *   id - The id of the element to update
      *   math - The Math to put into the element. this will replace the innerHTML of the element so it must
      *          be something that MathJax can process.
      *          For convenience ARTC.mathJax.tex will wrap TeX properly,
@@ -178,43 +103,20 @@ ARTC.mathJax.updateByElem2 = (function() {
      * success - a boolean indicating if everything went well
      * reason - a string which can be one of:
      *          "success" -> Indicating success, this is the only string where the success attribute will be true
-     *          "elem falsey" -> The elem was falsey
+     *          "id falsey" -> The id was falsey
      *          "math falsey" -> The math was falsey
+     *          "elem falsey" -> Could not find an element associated with the given id
      * details - an empty string on success or an error object given more details about the failure
      */
-    var update = function (elem, math, callback) {
-        var safeCallback = function () {  /* noop */; };
-        if (ARTC.isFunction(callback)) {
-            safeCallback = callback;
-        }
-
-        if (!elem) {
-            safeCallback({ success: false, reason: "elem falsey", details: "elem was " + elem });
+    var update = function (id, math, callback) {
+        if (!id) {
+            safeCallback({ success: false, reason: "id falsey", details: "id was " + id });
             return;
         }
 
-        if (!math) {
-            safeCallback({ success: false, reason: "math falsey", details: "math was " + math });
-            return;
-        }
+        var elem = document.getElementById(id);
 
-        elem.innerHTML = math;
-
-        //var oldDisplay = elem.style.display;
-        //if(oldDisplay == "") { oldDisplay = 'inline'; }
-        //if(oldDisplay == "none") {
-        //    MathJax.Hub.Queue(
-        //        [ "Typeset", MathJax.Hub, elem ],
-        //        safeCallback({ success: true, reason: "success", details: ""})
-        //    );
-        //} else {
-            MathJax.Hub.Queue(
-                //function () {elem.style.display='none'},
-                [ "Typeset", MathJax.Hub, elem ] //,
-                //function () {elem.style.display=oldDisplay},
-                //safeCallback({ success: true, reason: "success", details: ""})
-            );
-        //}
+        ARTC.mathJax.updateByElem(elem, math, callback);
     }
 
     return update;

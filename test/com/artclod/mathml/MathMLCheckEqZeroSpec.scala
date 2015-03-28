725c27d1066d862031e5be273f85de924dc846ac
Kristian Kime <kristian.kime@gmail.com>
2015-03-28 15:34:05 -0400

    version++

--- test/com/artclod/mathml/MathMLCheckEqZeroSpec.scala
+++ test/com/artclod/mathml/MathMLCheckEqZeroSpec.scala
@@ -22,6 +22,10 @@ class MathMLCheckEqZeroSpec extends Specification {
       ((log(2, `2`^x) - x) ?= `0`) must beEqualTo(Yes)
     }
 
+    "confirm log(10^x, 10) - x = 0" in {
+      ((log(10, `10`^x) - x) ?= `0`) must beEqualTo(Yes)
+    }
+
     "confirm log(2^x, 2) - x != 0.0000000001 (ensure non zero doesn't equal zero)" in {
       ((log(2, `2`^x) - x) ?= Cn(0.0000000001)) must beEqualTo(No)
     }

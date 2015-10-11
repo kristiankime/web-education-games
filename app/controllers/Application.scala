1280c18b71b292dcef9e57134594eea2103b8700
Kristian Kime <kristian.kime@gmail.com>
2015-10-11 13:35:35 -0400

    reving version

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -5,7 +5,7 @@ import java.lang.ref.WeakReference
 import play.api.mvc.{Action, Controller}
 
 object Application extends Controller {
-	val version = Version(0, 6, 7)
+	val version = Version(0, 7, 0)
 
   /**
 	 * Application does not use trailing slashes so indicate to browsers
